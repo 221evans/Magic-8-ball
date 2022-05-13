71e4539d7451ef864587e81c16ce6bc3053c9df9
221evans <104716319+221evans@users.noreply.github.com>
2022-05-13 23:29:57 +0100

    commit

---
+++ magic8ball.py
@@ -0,0 +1,26 @@
+import random
+random_number = random.randint(1,20)
+
+#variables
+name = "Jason"
+print(name)
+question = "Will I be a good programmer?"
+answer = ""
+
+# if and elif statements
+if random_number == 1:
+  answer = "It is certain."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Yes definitley."
+elif random_number == 5:
+  answer = "You may rely on it"
+elif random_number == 6:
+  answer = "Ah I see it, yes."
+elif random_number == 7:
+  answer = "Most likely."
+  
+
