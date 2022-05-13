6ef1b2b566a98a4dc9e00f400b89159179307595
221evans <104716319+221evans@users.noreply.github.com>
2022-05-13 23:43:54 +0100

    commit

--- magic8ball.py
+++ magic8ball.py
@@ -3,11 +3,11 @@ random_number = random.randint(1,20)
 
 #variables
 name = "Jason"
-print(name)
 question = "Will I be a good programmer?"
 answer = ""
 
 # if and elif statements
+
 if random_number == 1:
   answer = "It is certain."
 elif random_number == 2:
@@ -22,5 +22,29 @@ elif random_number == 6:
   answer = "Ah I see it, yes."
 elif random_number == 7:
   answer = "Most likely."
+elif random_number == 7:
+  answer == "Outlook good."
+elif random_number == 8:
+  answer = "Yes"
+elif random_number == 9:
+  answer = "Signs point to yes."
+elif random_number == 10:
+  answer = "Reply hazy, try again"
+elif random_number == 11:
+  answer = "Ask again later"
+elif random_number == 12:
+  answer = "Better not tell you now."
+elif random_number == 13:
+  answer = "Cannot predict now."
+elif random_number == 14:
+  answer = "Concentrate and ask again."
+elif random_number == 15:
+  answer = "Don't count on it."
+elif random_number == 16:
+  answer = "My reply in no."
+elif random_number == 17:
+  answer = "My sources say no."
+
+
   
 
